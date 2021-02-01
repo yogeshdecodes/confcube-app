c9ab919ddac0f2c79115967f7fcdbbd624854165
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-02-02 00:27:48 +0530

    fix 3 firebase blank screen

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,24 +12,24 @@ exports.onCreatePage = async ({ page, actions }) => {
     // createPage();
 }
 
-exports.onCreateWebpackConfig = ({
-  stage,
-  actions,
-  getConfig
-}) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function(context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/;
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-        if (regex.test(request)) {
-          return callback(null, 'umd ' + request);
-        }
-        callback();
-      })
-    });
-  }
-};
+// exports.onCreateWebpackConfig = ({
+//   stage,
+//   actions,
+//   getConfig
+// }) => {
+//   if (stage === 'build-html') {
+//     actions.setWebpackConfig({
+//       externals: getConfig().externals.concat(function(context, request, callback) {
+//         const regex = /^@?firebase(\/(.+))?/;
+//         // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+//         if (regex.test(request)) {
+//           return callback(null, 'umd ' + request);
+//         }
+//         callback();
+//       })
+//     });
+//   }
+// };
 
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
