833b830418a139ef3a273352251461accc4b4203
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-02-01 23:16:57 +0530

    gatsby link prop ssr error fix

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -8,13 +8,15 @@ import PrivateRoute from "../components/PrivateRoute"
 import Status from "../components/Status"
 
 const App = (props) => {
+ const eventdetails="";
+  if (typeof window !== 'undefined') {eventdetails=props.location.state.EventDetails}
  
   return(
   <Layout>
     <Status />
     <Router>
       <PrivateRoute path="/app/profile" component={Profile} />
-      <PrivateRoute path="/app/detail" component={Detail} EventDetails={props.location.state.EventDetails} />
+      <PrivateRoute path="/app/detail" component={Detail} EventDetails={eventdetails} />
       <Login path="/app/login" />
     </Router>
   </Layout>
