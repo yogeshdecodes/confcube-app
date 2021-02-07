85807e55516575aaa14b99fbc1cfe44cabc272f3
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-02-07 14:01:51 +0530

    fix: build failed

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,26 @@ exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
 }
 
+exports.onCreateWebpackConfig = ({
+  stage,
+  actions,
+  getConfig
+}) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      externals: getConfig().externals.concat(function(context, request, callback) {
+        const regex = /^@?firebase(\/(.+))?/;
+        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+        if (regex.test(request)) {
+          return callback(null, 'umd ' + request);
+        }
+        callback();
+      })
+    });
+  }
+};
+
+
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html") {
     actions.setWebpackConfig({
@@ -13,23 +33,6 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
           },
         ],
       },
-    })
-  }
-}
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function (
-        _context,
-        request,
-        callback
-      ) {
-        if (/^@?(react-)?firebase(.*)/.test(request)) {
-          console.log("Excluding bundling of: " + request)
-          return callback(null, "umd " + request)
-        }
-        callback()
-      }),
     })
   }
 }
