766ee1ec3b9ab356c0f191597bb895353f11cdec
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-02-07 09:48:10 +0530

    feat: firebase database serverless functions

---
+++ src/utils/db.js
@@ -0,0 +1,72 @@
+import firebase from "gatsby-plugin-firebase"
+ 
+
+export const isBrowser = () => typeof window !== "undefined"
+let firestore={};
+isBrowser() && (firestore = firebase.firestore())
+
+export function createUser(uid, data, defaultPhotoUrl ) {
+  return firestore
+    .collection('users')
+    .doc(uid)
+    .set({ uid, ...data, defaultPhotoUrl }, { merge: true });
+}
+
+
+export async function createPublicUser(uid, displayName, defaultPhotoUrl, email) {
+ const docRef= await firestore
+    .collection('publicusers')
+    .doc(uid).get()
+
+    if(!docRef.exists)
+    {
+    firestore
+    .collection('publicusers')
+    .doc(uid).set({ avatarUrl:defaultPhotoUrl ,name:displayName, email:email, bio:"", mySkills:"", lookingFor:"", interests:"", events:[]}, { merge: true });
+    }
+}
+
+
+export async function getPublicUser(uid) {
+    const doc = await firestore.collection('publicusers').doc(uid).get()
+    const site = {  ...doc.data() }; 
+    return site ; 
+}
+
+ 
+export function updatePublicUser(uid, name, bio, mySkills, interests,  lookingFor) {
+ const docRef= firestore
+    .collection('publicusers')
+    .doc(uid)
+      
+    docRef.set({ name:name, bio:bio, mySkills:mySkills, interests:interests, lookingFor:lookingFor},{ merge: true });
+    return docRef;
+}
+ 
+
+export async function getEvent(uid) {
+const getEventref= await firestore.collection('publicusers').where(firebase.firestore.FieldPath.documentId(), '==', `${uid}`).get();
+return getEventref;  
+}
+
+
+export async function eventUser(id) { 
+const doc = await firestore.collection("publicusers").where("events", "array-contains", `${id}`).get()
+return doc; 
+}
+
+ 
+
+export async function addEvent(uid, id) { 
+var eventRef = firestore.collection("publicusers").doc(uid);
+eventRef.update({
+    events: firebase.firestore.FieldValue.arrayUnion(`${id}`)
+}); 
+}
+
+export async function removeEvent(uid, id) { 
+var eventRef = firestore.collection("publicusers").doc(uid);
+eventRef.update({
+    events: firebase.firestore.FieldValue.arrayRemove(`${id}`)
+});
+}
