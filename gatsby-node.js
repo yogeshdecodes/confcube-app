00b60c4417c457f939092bb91e0f07ec39a6a454
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-02-07 14:29:59 +0530

    refactor: remove unneccessary

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,3 @@
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
-}
-
-exports.onCreateWebpackConfig = ({
-  stage,
-  actions,
-  getConfig
-}) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function(context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/;
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
-        if (regex.test(request)) {
-          return callback(null, 'umd ' + request);
-        }
-        callback();
-      })
-    });
-  }
-};
-
-
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html") {
     actions.setWebpackConfig({
@@ -35,4 +11,8 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       },
     })
   }
+}
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
 }
